f7a2f83e32dc2dba88f9850d3b575427ab5debce
Emily Sperry <sperrye@gmail.com>
2020-10-07 02:39:46 -0700

    finish linter setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
-  rules: {},
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: ['airbnb-typescript', 'prettier', 'prettier/react', 'prettier/@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
   ignorePatterns: ['dist', '.storybook', 'node_modules', '*.stories.js'],
 };
